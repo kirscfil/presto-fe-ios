e8ae485b7ef2a1018611ae8464898c6aad263395
Filip Kirschner <filip.kirschner@applifting.cz>
2017-09-26 14:58:03 +0200

    Initial Commit

---
+++ PrestoMenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PrestoMenu
+//
+//  Created by Filip Kirschner on 26/09/2017.
+//  Copyright © 2017 Applifting. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
